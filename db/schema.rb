a4e53e901f003adc5e0e0f3bc0d6e006b702ade3
kevin robert <kevin.j.robert@gmail.com>
2016-01-08 12:24:01 +0100

    setup github connect

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160108111348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "teachers", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "company_name"
+    t.string   "company_status"
+    t.string   "siret"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
 end
