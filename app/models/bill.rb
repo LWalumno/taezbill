bf273f09dd0a9ebd0b5010c9d3b65f7623e742ec
kevin robert <kevin.j.robert@gmail.com>
2016-01-11 13:15:28 +0100

    design bill show

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,5 +2,8 @@ class Bill < ActiveRecord::Base
   belongs_to :teacher
   has_many :items
   has_many :item_fields, through: :items
-  has_many :fields, through: :item_fields
+  has_many :fields,  -> { distinct }, through: :item_fields
+
+  validates :name, presence: true
+  validates :date, presence: true
 end
