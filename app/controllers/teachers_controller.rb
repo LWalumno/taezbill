e366f9a1eb00d09e83bf27f0b406afa611de2366
kevin robert <kevin.j.robert@gmail.com>
2016-01-15 16:04:06 +0100

    problem routing to sign_in

---
+++ app/controllers/teachers_controller.rb
@@ -0,0 +1,7 @@
+class TeachersController < ApplicationController
+
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback" => "sessions#create"
   get "/sign_out" => "sessions#destroy"
   resources :bills
+  resources :teachers, only: [:edit, :update]
   root to: "bills#new"
 end
