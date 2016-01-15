dfe532b5914169ec2bfe0ef82274fb2c16cdc682
kevin robert <kevin.j.robert@gmail.com>
2016-01-16 00:00:14 +0100

    more design

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -7,7 +7,7 @@ class BillsController < ApplicationController
     year = date.strftime("%Y")
     str_date = date.to_s
     name = "Facture missions TA #{month} #{year}"
-    number = str_date.split("-").join("") + "00001"
+    number = "#{year}#{date.strftime('%m')}00001"
     @bill = Bill.new(name: name, number: number, date: str_date)
   end
 
