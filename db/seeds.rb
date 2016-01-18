857ee46724e1f2ee451de65806204c959ca64c92
kevin robert <kevin.j.robert@gmail.com>
2016-01-18 12:01:50 +0100

    save seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,27 @@
-# Field.destroy_all
+Field.destroy_all
 
-# fields_attributes = [
-#   {
-#     name: "Description",
-#     unit: nil
-#   },
-#   {
-#     name: "Daily price",
-#     unit: "€"
-#   },
-#   {
-#     name: "Quantity",
-#     unit: nil
-#   },
-#   {
-#     name: "Price",
-#     unit: "€"
-#   }
-# ]
+fields_attributes = [
+  {
+    name: "Description",
+    unit: nil
+  },
+  {
+    name: "Daily price",
+    unit: "€"
+  },
+  {
+    name: "Quantity",
+    unit: nil
+  },
+  {
+    name: "Price",
+    unit: "€"
+  }
+]
 
-# fields_attributes.each do |attr|
-#   Field.create!(attr)
-# end
+fields_attributes.each do |attr|
+  Field.create!(attr)
+end
 
 Customer.destroy_all
 lewagon = Customer.create!(
