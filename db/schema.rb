f0fa27368b11edaa97fb2de76e671304c149bdb2
kevin robert <kevin.j.robert@gmail.com>
2016-01-18 09:54:23 +0100

    add customers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115230254) do
+ActiveRecord::Schema.define(version: 20160118085345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20160115230254) do
   end
 
   add_index "bills", ["teacher_id"], name: "index_bills_on_teacher_id", using: :btree
+
+  create_table "customers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "company_name"
+    t.string   "address"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "fields", force: :cascade do |t|
     t.string   "name"
