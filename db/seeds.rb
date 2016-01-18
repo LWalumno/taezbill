127522fe971596d2433cc6afa29d52645bda3d0b
kevin robert <kevin.j.robert@gmail.com>
2016-01-18 11:54:52 +0100

    add lewagon

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,30 @@
-Field.destroy_all
+# Field.destroy_all
 
-fields_attributes = [
-  {
-    name: "Description",
-    unit: nil
-  },
-  {
-    name: "Daily price",
-    unit: "€"
-  },
-  {
-    name: "Quantity",
-    unit: nil
-  },
-  {
-    name: "Price",
-    unit: "€"
-  }
-]
+# fields_attributes = [
+#   {
+#     name: "Description",
+#     unit: nil
+#   },
+#   {
+#     name: "Daily price",
+#     unit: "€"
+#   },
+#   {
+#     name: "Quantity",
+#     unit: nil
+#   },
+#   {
+#     name: "Price",
+#     unit: "€"
+#   }
+# ]
 
-fields_attributes.each do |attr|
-  Field.create!(attr)
-end
+# fields_attributes.each do |attr|
+#   Field.create!(attr)
+# end
+
+Customer.destroy_all
+lewagon = Customer.create!(
+  name: "Le Wagon",
+  company_name: "Lewagon",
+  address: "24 rue Louis Blanc, 75010, Paris")
